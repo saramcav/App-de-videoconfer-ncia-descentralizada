0e7bb09ee603a0fc9ea0cb2ac44afdeb47439369
SerranoZz <serrano@Serrano>
2023-10-12 19:17:11 -0300

    versao final etapa 1

--- cliente.py
+++ cliente.py
@@ -2,17 +2,18 @@ import socket
 from utils import *
 
 class Client:
+    #Classe cliente com as suas respectivas informações de nome, porta e ip
     def __init__(self, server_host, server_port, name, ip, reception_port):
         self._name = name
         self._ip = ip
         self._reception_port = reception_port
-        addr = (server_host, server_port)
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.connect(addr)
+        addr = (server_host, server_port) #criação do par ip-porta do servidor
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #criação do socket tcp do cliente
+        self._socket.connect(addr) #solicita a conexão com o socket tcp do servidor
         print(f'[CONEXAO] cliente conectado ao servidor em {server_host}:{server_port}\n')
         self.run()
 
-    def enconde_message(self, message):
+    def encode_message(self, message): #função que cria a mensagem associada à opção escolhida no menu para ser enviada ao servidor
         if message == '1':
             message = f'{USER_QUERY_MSG}::='
             message += str(input(f'Insira o usuário a ser buscado> '))
@@ -25,13 +26,13 @@ class Client:
 
         return message
 
-    def decode_message(self, message):
+    def decode_message(self, message): #função para decodificar uma mensagem recebida do servidor
         msg = message.split("::=")
         return msg
     
     def run(self):    
-        msg = f'{NEW_REGISTER_MSG}::={self._name},{self._ip},{self._reception_port}'
-        self._socket.send(msg.encode(FORMAT))
+        msg = f'{NEW_REGISTER_MSG}::={self._name},{self._ip},{self._reception_port}' #mensagem de registro 
+        self._socket.send(msg.encode(FORMAT)) #função que envia a mensagem de registro para o servidor
 
         print('Para realizar uma consulta pelo nome de usuário, insira \'1\'')
         print('Para se desvincular do servidor, insira \'2\'')
@@ -39,19 +40,19 @@ class Client:
 
         conectado = True
         while conectado:
-            msg = self._socket.recv(SIZE).decode(FORMAT)
-            msg = self.decode_message(msg)
+            msg = self._socket.recv(SIZE).decode(FORMAT) #função que fica ouvindo o servidor 
+            msg = self.decode_message(msg) 
             print(f'[SERVIDOR]: {msg[1]}')
 
-            if msg[0] == DISCONNECT_MSG:
+            if msg[0] == DISCONNECT_MSG: #se receber uma mensagem do servidor que a conexão foi encerrada, ele sai do loop
                 conectado = False
 
             else:
-                msg = str(input(f'> '))
-                msg = self.enconde_message(msg)
-                self._socket.send(msg.encode(FORMAT))
+                msg = str(input(f'> ')) #input para a escolha das mensagens que são enviadas ao servidor
+                msg = self.encode_message(msg)
+                self._socket.send(msg.encode(FORMAT))  #função que envia a mensagem de registro para o servidor
 
-        self._socket.close()
+        self._socket.close() #função que fecha o socket e finaliza o processo do cliente
 
 def main():
     nome = str(input(f'Insira o seu nome>'))
@@ -59,7 +60,6 @@ def main():
     porta = str(input(f'Insira a sua porta>'))
 
     cliente = Client(host, port, nome, endereco, porta)
-
 
 
 if __name__ == "__main__":
--- servidor.py
+++ servidor.py
@@ -16,62 +16,61 @@ class ClientRegister:
 class Server:
     def __init__(self, host, port):
         self._clients = {}
-        self._clients['A'] = ClientRegister('22', '33')
         addr = (host, port)
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.bind(addr)
-        self._socket.listen()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #criação do socket tcp do cliente
+        self._socket.bind(addr) #vincula um par ip-porta para o socket do servidor
+        self._socket.listen() #servidor fica escutando as solicitações dos sockets clientes
         print(f'O servidor está ouvindo em {host}:{port}')
         self.run()
     
     def run(self):
         while True:
-            conn, addr = self._socket.accept()
-            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            conn, addr = self._socket.accept() #aceita a conexão com um socket cliente
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr)) #cria uma thread para esse socket client
             thread.start()
             print(f'[CONEXOES ATIVAS] {threading.active_count() - 1}')
     
     def handle_client(self, conn, addr):
         conectado = True
         while conectado:
-            msg = conn.recv(SIZE).decode(FORMAT)
+            msg = conn.recv(SIZE).decode(FORMAT) #função que fica ouvindo o socket cliente
             print(f'[{addr}] {msg}')
             msg = self.decode_message(msg)
 
-            if msg[0] == DISCONNECT_MSG:
+            if msg[0] == DISCONNECT_MSG: #se a mensagem recebida for para desconectar o cliente
                 client = msg[1]
                 print(f'[CLIENTE DESCONECTADO] {client} deixou o servidor ...')
-                print(f'[CONEXOES ATIVAS] {threading.active_count() - 1}')
-                msg = f'{DISCONNECT_MSG}::=VOCÊ FOI DESCONECTADO!'
-                conectado = False
+                print(f'[CONEXOES ATIVAS] {threading.active_count() - 2}')
+                msg = f'{DISCONNECT_MSG}::=VOCÊ FOI DESCONECTADO!' #mensagem de retorno para o cliente
+                conectado = False #bota vairável = false para poder sair do loop
 
-            elif msg[0] == NEW_REGISTER_MSG:
+            elif msg[0] == NEW_REGISTER_MSG: #se a mensagem recebida for de um novo registro
                 name, ip, reception_port = msg[1].split(',')
-                msg = self.add_client_register(name, ip, reception_port)
-                print(self.clients_table())
+                msg = self.add_client_register(name, ip, reception_port) #mensagem de retorno para o cliente
+                print(self.clients_table()) #printa a tabela de clientes
             
-            elif msg[0] == USER_QUERY_MSG:
+            elif msg[0] == USER_QUERY_MSG: #se a mensagem recebida for uma busca de outro usuário
                 name = msg[1]
-                msg = f'{USER_QUERY_MSG}::={self.query_user(name)}'
+                msg = f'{USER_QUERY_MSG}::={self.query_user(name)}' #mensagem de retorno para o cliente
 
             elif msg[0] == TABLE_QUERY_MSG:
                 name = msg[1]
-                msg = f'{TABLE_QUERY_MSG}::={self.clients_table()}'
+                msg = f'{TABLE_QUERY_MSG}::={self.clients_table()}' #mensagem de retorno para o cliente
 
             else:
-                msg = "[WARNING]::=TENTE NOVAMENTE"
+                msg = "[WARNING]::=OPÇÃO INVÁLIDA" #mensagem de retorno para o cliente
             
-            conn.send(msg.encode(FORMAT))
+            conn.send(msg.encode(FORMAT)) #função que envia a mensagem resposta para o cliente
             
-        conn.close()
+        conn.close() #função que encerra a conexão com o socket cliente
     
     
-    def decode_message(self, message):
+    def decode_message(self, message): #função para decodificar uma mensagem recebida do cliente
         msg = message.split("::=")
         return msg
         
             
-    def add_client_register(self, name, ip, reception_port):
+    def add_client_register(self, name, ip, reception_port): #função que adiciona um novo registro de cliente no servidor e retorna uma mensagem
         if self._clients.get(name) is None: 
             self._clients[name] = ClientRegister(ip, reception_port)
             msg = f'[SUCESSO NO REGISTRO]::={name} inserido/a!'
@@ -80,7 +79,7 @@ class Server:
         return msg
 
 
-    def query_user(self, client_name):
+    def query_user(self, client_name): #função que busca um nome de usuário solicitado pelo cliente e retorna uma mensagem
         client = self._clients.get(client_name)
 
         if client is None:
@@ -89,7 +88,7 @@ class Server:
             msg = f'NOME: {client_name:<12} | IP: {client.get_ip():<20} | RECEPTION_PORT: {client.get_reception_port():<6}'
         return msg
     
-    def clients_table(self):
+    def clients_table(self): #função que retorna a tabela de todos os clientes conectados no servidor como mensagem
         msg = f'{"NOME":<12} | {"IP":<20} | {"PORTA":<6}\n'
         msg += '-' * 60 + '\n'
         for name in self._clients:
